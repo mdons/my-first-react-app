c6deaab853f3e14a7e425a3e73bcac318c5ea782
mdons <michaeljdonnell@gmail.com>
2019-01-23 22:50:21 -0600

    backing up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import FirstComponent from "./firstComponent";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      firstNames: [],
+      lastNames: []
+    };
+  }
+
+  // toggle = () => {
+  //   this.setState({ isOn: !this.state.isOn });
+  // };
+
+  inputUpdate1 = event => {
+    this.setState({ firstName: event.target.value });
+  };
+
+  inputUpdate2 = event => {
+    this.setState({ lastName: event.target.value });
+  };
+
+  formSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      firstNames: [...this.state.firstNames, this.state.firstName],
+      lastNames: [...this.state.lastNames, this.state.lastName],
+      firstName: "",
+      lastName: ""
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <form onSubmit={this.formSubmit}>
+            <input
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.inputUpdate1}
+            />
+            <input
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.inputUpdate2}
+            />
+            <button>Submit</button>
+          </form>
+          <FirstComponent items={this.state.firstNames} />
+          <FirstComponent items={this.state.lastNames} />
         </header>
       </div>
     );
